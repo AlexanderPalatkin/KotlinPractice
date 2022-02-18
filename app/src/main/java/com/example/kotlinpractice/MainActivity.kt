a24159b22363b42dd9469a4ce6d619ecc5e9aaaa
AlexanderPalatkin <palatkin.alexander@gmail.com>
2022-02-18 15:26:10 +0300

    добавил кнопку в разметку и прикрепил листенер создал data class Profile и отобразил данные класса на экран при нажатии на кнопку

--- app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.kotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mainTextView = findViewById<TextView>(R.id.main_text_view)
+
+        val profile = Profile()
+
+        findViewById<Button>(R.id.main_button).setOnClickListener {
+            mainTextView.text = "name: " + profile.name + " age: " + profile.age
+        }
     }
 }
